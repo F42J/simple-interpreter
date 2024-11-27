e4f731aa08a9a3c05aeecd36f4260d64ec3e1ec9
Jakob Fuchs <ja.fuchs@tum.de>
2024-11-27 22:58:01 +0100

    added sample and fixed bug in commandline parser

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,11 +3,11 @@ package org.example
 import java.io.File
 
 fun main(args: Array<String>) {
-    if (args.size != 2) {
+    if (args.size != 1) {
         println("Usage: java -jar example.jar  <input> ")
         return
     }
-    val path = args[1]
+    val path = args[0]
     val program = File(path)
     val interpreter = Interpreter()
     val res = interpreter.execute(program.readLines())
